b351fee19071d9b5f84dd89aad3f96bc38d5a857
Alex Shnyrov <1993alexey@gmail.com>
2020-09-14 14:20:31 -0600

    Added homepage

---
+++ web/header/header.php
@@ -0,0 +1,17 @@
+<div class="menu-container">
+    <ul id="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="about.php">About </a></li>
+    <li>
+      <a href="#">Assignments</a>
+      <ul class="hidden">
+        <li><a href="future-assignment.php">Week 2</a></li>
+        <li><a href="future-assignment.php">Week 3</a></li>
+        <li><a href="future-assignment.php">Week 4</a></li>     
+        <li><a href="future-assignment.php">Week 5</a></li>  
+        <li><a href="future-assignment.php">Week 6</a></li>  
+        <li><a href="future-assignment.php">Week 7</a></li>     
+      </ul>
+    </li>
+    </ul>
+</div>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,13 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="header/header.css">
+</head>
+<body>
+  <?php include 'header/header.php';?>
+  <h1>Welcome home!</h1>
+</body>
+</html>
