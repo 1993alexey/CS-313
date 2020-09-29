cb213944cb555f4732dd1ab6003eb4315159931d
Alex Shnyrov <1993alexey@gmail.com>
2020-09-28 22:45:27 -0600

    Added public dir

---
+++ backend/public/precache-manifest.d0af22310528e93178746fc3216f4a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d206503231dc33abe51908ddd6ff71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264073bff51f3b0065db",
+    "url": "/static/css/main.70f6c6fb.chunk.css"
+  },
+  {
+    "revision": "7bffbb3893f1db6536c7",
+    "url": "/static/js/2.1d92917e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1d92917e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264073bff51f3b0065db",
+    "url": "/static/js/main.f3ff930a.chunk.js"
+  },
+  {
+    "revision": "ce78dafce621d1a246b2",
+    "url": "/static/js/runtime-main.9e9e7a2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
