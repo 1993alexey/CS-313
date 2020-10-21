2495202fac4c70d979591dbf1050c9b339953d1e
Alex Shnyrov <alex.shnyrov@legrand.us>
2020-10-21 12:35:13 -0600

    Rebuilt frontend

---
+++ backend/public/precache-manifest.e9d7d3799912d9d5a626744b2ad20b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fa4bf7d2702cad1837696ff37059b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00463d59447519b0f16",
+    "url": "/static/css/main.5d66dbf1.chunk.css"
+  },
+  {
+    "revision": "e57895d40e8b436555c0",
+    "url": "/static/js/2.7fa199fc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7fa199fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00463d59447519b0f16",
+    "url": "/static/js/main.b3b6fb12.chunk.js"
+  },
+  {
+    "revision": "ce78dafce621d1a246b2",
+    "url": "/static/js/runtime-main.9e9e7a2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
